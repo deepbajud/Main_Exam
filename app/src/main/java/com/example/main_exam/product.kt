96ee73474181ff843df6b31fbeb2c6ce5c976928
mansi-kathiriya <mansikathiriya08@gmail.com>
2023-09-26 13:03:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/main_exam/product.kt
@@ -0,0 +1,11 @@
+package com.example.main_exam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class product : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product)
+    }
+}
---
+++ app/src/main/java/com/example/main_exam/splashscreen.kt
@@ -0,0 +1,40 @@
+package com.example.main_exam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.annotation.SuppressLint
+
+import android.content.Intent
+import android.os.Handler
+import android.widget.TextView
+import com.airbnb.lottie.LottieAnimationView
+
+class SplashScreen : AppCompatActivity() {
+    private lateinit var appname: TextView
+    private lateinit var lottie: LottieAnimationView
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+        appname = findViewById(R.id.credit)
+        lottie = findViewById(R.id.lottie)
+
+        appname.animate()
+            .translationY(-1400f)
+            .setDuration(2700)
+            .setStartDelay(0)
+
+        lottie.animate()
+            .translationX(2000f)
+            .setDuration(2000)
+            .setStartDelay(2900)
+
+        Handler().postDelayed({
+            val i = Intent(applicationContext, MainActivity::class.java)
+            startActivity(i)
+            finish()
+        }, 5000)
+    }
+}
